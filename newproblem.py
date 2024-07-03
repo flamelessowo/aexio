75e94f122ba3b5072579cc5c0afef29e2fc5f636
Christian <flamelessowo@gmail.com>
2024-07-03 18:33:59 +0300

    make async sleep

--- newproblem.py
+++ newproblem.py
@@ -1,17 +1,33 @@
 import time
 from collections import deque
-
+import heapq
 
 class Scheduler:
     def __init__(self) -> None:
         self.ready = deque()
+        self.sleeping = []
         self.current = None # Currently executing generator
+        self.sequence = 0
+
+    async def sleep(self, delay):
+        # Current coro wants to sleep
+        deadline = time.time() + delay
+        self.sequence += 1
+        heapq.heappush(self.sleeping, (deadline, self.sequence, self.current))
+        self.current = None # Disappear
+        await switch() # Switch tasks
 
     def new_task(self, coro):
         self.ready.append(coro)
 
     def run(self):
-        while self.ready:
+        while self.ready or self.sleeping:
+            if not self.ready:
+                deadline, _, coro = heapq.heappop(self.sleeping)
+                delta = deadline - time.time()
+                if delta > 0:
+                    time.sleep(delta)
+                self.ready.append(coro)
             self.current = self.ready.popleft()
             # Drive as a generator
             try:
@@ -35,20 +51,18 @@ def switch():
 async def countdown(n):
     while n > 0:
         print('Down', n)
-        time.sleep(1)
-        await switch() # Switch tasks
+        await sched.sleep(4)
         n -= 1
 
 async def countup(stop):
     x = 0
     while x < stop:
         print('Up', x)
-        time.sleep(1)
-        await switch()
+        await sched.sleep(1)
         x += 1
 
 sched.new_task(countdown(5))
-sched.new_task(countup(5))
+sched.new_task(countup(20))
 sched.run()
 
 # In new python verisons they tried to hide yield from the user (async/await)
