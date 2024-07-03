6f6905d93b082f600ae8d9b45e677bca7dbf3132
Christian <flamelessowo@gmail.com>
2024-07-03 12:07:31 +0300

    Make use of closed queue instead of None hack

--- aproducer.py
+++ aproducer.py
@@ -10,12 +10,33 @@ from collections import deque
 sched = Scheduler()
 
 
+class Result:
+    def __init__(self, value=None, exc=None) -> None:
+        self.value = value
+        self.exc = exc
+
+    def result(self):
+        if self.exc:
+            raise self.exc
+        else:
+            return self.value
+
+
 class AsyncQueue:
     def __init__(self) -> None:
         self.items = deque()
         self.waiting = deque() # All getters waiting for data
+        self._closed = False # Can queue be used anymore?
+
+    def close(self):
+        self._closed = True
+        if self.waiting and not self.items:
+            for func in self.waiting:
+                sched.call_soon(func)
 
     def put(self, item):
+        if self._closed:
+            raise RuntimeError('Queue closed')
         self.items.append(item)
         if self.waiting:
             func = self.waiting.popleft()
@@ -25,10 +46,16 @@ class AsyncQueue:
 
     def get(self, callback):
         # Wait till item available. Then return
+        # How does a closed queue interact with get()
         if self.items:
-            callback(self.items.popleft())
+            callback(Result(value=self.items.popleft())) # Good result
         else:
-            self.waiting.append(lambda: self.get(callback))
+            # No items available (must wait)
+            if self._closed:
+                # Now what?
+                callback(Result(exc=RuntimeError('Queue closed'))) # error result
+            else:
+                self.waiting.append(lambda: self.get(callback))
 
 
 
@@ -40,16 +67,17 @@ def producer(q, count):
             sched.call_later(1, lambda: _run(n+1))
         else:
             print('Producer done')
-            q.put(None)
+            q.close() # No more items produced
     _run(0)
 
 def consumer(q):
-    def _consume(item):
-        if item is None:
-            print('Consumer done')
-        else:
-            print('Consuming', item)
+    def _consume(result):
+        try:
+            item = result.result()
+            print('Consuming', item) # <<<<< Queue item result
             sched.call_soon(lambda: consumer(q))
+        except RuntimeError:
+            print("Consumer done")
     q.get(callback=_consume)
 
 q = AsyncQueue()
