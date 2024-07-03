78217e7a0126dd2752b023866c55ce9d3ae7d923
Christian <flamelessowo@gmail.com>
2024-07-03 12:26:20 +0300

    Make use of python async await and coroutines

--- newproducer.py
+++ newproducer.py
@@ -7,15 +7,15 @@ class Scheduler:
         self.ready = deque()
         self.current = None # Currently executing generator
 
-    def new_task(self, gen):
-        self.ready.append(gen)
+    def new_task(self, coro):
+        self.ready.append(coro)
 
     def run(self):
         while self.ready:
             self.current = self.ready.popleft()
             # Drive as a generator
             try:
-                next(self.current)
+                self.current.send(None) # Send to a coroutine
                 if self.current:
                     self.ready.append(self.current)
             except StopIteration:
@@ -23,21 +23,32 @@ class Scheduler:
 
 sched = Scheduler() # Background scheduler object
 
-def countdown(n):
+
+class Awaitable:
+    def __await__(self):
+        yield
+
+
+def switch():
+    return Awaitable()
+
+async def countdown(n):
     while n > 0:
         print('Down', n)
         time.sleep(1)
-        yield
+        await switch() # Switch tasks
         n -= 1
 
-def countup(stop):
+async def countup(stop):
     x = 0
     while x < stop:
         print('Up', x)
         time.sleep(1)
-        yield
+        await switch()
         x += 1
 
 sched.new_task(countdown(5))
 sched.new_task(countup(5))
 sched.run()
+
+# In new python verisons they tried to hide yield from the user (async/await)
